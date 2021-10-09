998ced7a73b4258ecade2f554cbf2294fef77508
kl3mousse <kl3mousse@gmail.com>
2021-10-09 11:12:43 +0200

    button to download test as picture + code cleansing

--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@
 
 
 import "./styles.css";
-import qrcodeapplink from "./qrcode-app-link.gif"
-import React, { useEffect } from "react";
-//import ReactDOM from 'react-dom'
-import Favicon from 'react-favicon'
+import qrcodeapplink from "./qrcode-app-link.gif";
+import React, { createRef } from "react";
+import { useScreenshot, createFileName } from "use-react-screenshot";
 
 import {
   AppBar,
@@ -22,7 +21,7 @@ import {
   TextField,
   useMediaQuery,
   Tooltip,
-  Card, CardContent, CardMedia, Link
+  Card, CardMedia, Link
 } from "@material-ui/core";
 import Dialog from "@material-ui/core/Dialog";
 import Box from "@material-ui/core/Box";
@@ -37,9 +36,10 @@ import FeedbackTwoToneIcon from '@material-ui/icons/FeedbackTwoTone';
 import HelpTwoToneIcon from '@material-ui/icons/HelpTwoTone';
 import AddCircleTwoToneIcon from "@material-ui/icons/AddCircleTwoTone";
 import CopyrightTwoToneIcon from "@material-ui/icons/CopyrightTwoTone";
+import SaveTwoToneIcon from '@material-ui/icons/SaveTwoTone';
 import ShareIcon from '@material-ui/icons/Share';
 
-import { alpha, makeStyles, ThemeProvider, useTheme, createTheme } from "@material-ui/core/styles";
+import { useTheme } from "@material-ui/core/styles";
 
 // Google Analytics
 import ReactGA from 'react-ga';
@@ -75,7 +75,7 @@ export function FeedbackFormDialog() {
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfCD3hg_ACPlpMnPdf_DyKiAk4uofXhwlcreVrR203JoyI6dA/viewform?embedded=true" width="500" height="3062" frameborder="0" marginheight="0" marginwidth="0" > Loading…</iframe>
+        <iframe title="feedbackform" src="https://docs.google.com/forms/d/e/1FAIpQLSfCD3hg_ACPlpMnPdf_DyKiAk4uofXhwlcreVrR203JoyI6dA/viewform?embedded=true" width="500" height="3062" frameborder="0" marginheight="0" marginwidth="0" > Loading…</iframe>
         <DialogActions>
           <Button onClick={handleClose} color="primary" autoFocus>
             Fermer
@@ -133,7 +133,7 @@ export function AlertDialog() {
             voir l'évolution de votre déficit, par exemple toutes les semaines.
           </DialogContentText>
           <DialogContentText id="alert-dialog-description3">
-            version 1.2.0
+            version 1.3.0
           </DialogContentText>
           <DialogContentText id="alert-dialog-description4">
             merci au Dr Flabeau du Centre Hospitalier de la Côte Basque pour l'idée de l'app.
@@ -188,10 +188,6 @@ export function CopyrightDialog() {
           </DialogContentText>
           <DialogContentText>
             LE LOGICIEL EST FOURNI "EN L'ETAT", SANS AUCUNE GARANTIE, EXPRESSE OU IMPLICITE, Y COMPRIS DE MANIÈRE NON LIMITÉE À LA GARANTIE DE QUALITÉ MARCHANDE, D'ADÉQUATION À UN USAGE PARTICULIER ET D'INFRACTION. EN AUCUN CAS, LES AUTEURS OU LES TITULAIRES DE COPYRIGHT NE PEUVENT ÊTRE TENUS RESPONSABLES DE TOUTE RÉCLAMATION, DE DOMMAGES OU D'AUTRE RESPONSABILITÉ, QU'IL SOIT PAR UN ACTION DE CONTRAT, DE LORT OU DE AUTRE QUE CE SOIT, OU LIÉ AU LOGICIEL OU À L'UTILISATION OU AUTRE LOGICIEL.
-          </DialogContentText>
-          <DialogContentText>
-            credits: icône de l'app tirée de...
-            <a href="https://icons8.com/icon/mHBAv5xJ2dcq/eye">Icons8</a>
           </DialogContentText>
         </DialogContent>
         <DialogActions>
@@ -389,7 +385,6 @@ class Game extends React.Component {
     const history = this.state.history;
     const current = history[history.length - 1];
     const squares = current.squares.slice();
-    this.state.nbClickedBoxes++;
 
     if (this.state.isFirstSquareSet) {
       squares[i] = COLOR2;
@@ -402,23 +397,26 @@ class Game extends React.Component {
           squares: squares
         }
       ]),
-      isFirstSquareSet: true
+      isFirstSquareSet: true,
+      nbClickedBoxes: this.state.nbClickedBoxes + 1
     });
   }
 
   handleUndo() {
     var boardHistory = this.state.history;
     boardHistory.pop(); // removes last element from array :)
-    this.state.nbClickedBoxes--;
+
     let isFirstSquareSet = false;
     if (boardHistory.length === 1) {
       isFirstSquareSet = false;
     } else {
       isFirstSquareSet = true;
     }
+
     this.setState({
       history: boardHistory,
-      isFirstSquareSet: isFirstSquareSet
+      isFirstSquareSet: isFirstSquareSet,
+      nbClickedBoxes: this.state.nbClickedBoxes - 1
     });
   }
 
@@ -431,25 +429,10 @@ class Game extends React.Component {
   render() {
     const history = this.state.history;
     const current = history[history.length - 1];
-    const defaultDate = new Date();
-    const defaultDateTimeValue = defaultDate.getUTCFullYear;
 
     return (
       <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-              <VisibilityTwoToneIcon />
-            </IconButton>
-            <Typography variant="h6" >JVoiPa</Typography>
-            <Box sx={{ flexGrow: 1 }} />
-            <FeedbackFormDialog />
-            <AlertDialog />
-            <CopyrightDialog />
-            <ShareDialog />
-          </Toolbar>
 
-        </AppBar>
 
         <Container className="game" maxWidth="xl">
           <Grid
@@ -592,14 +575,41 @@ function useWidth() {
 
 export default function App() {
   const appWidth = useWidth();
-
-  // set the App title (the one visible in browser tabs)
-  useEffect(() => {
-    document.title = "JvoiPa"
-  }, [])
+  const ref = createRef(null);
+  const [ , takeScreenshot] = useScreenshot();
+  //const getImage = () => takeScreenshot(ref.current);
+  const download = (image, { name = "JvoiPa-", extension = "jpg" } = {}) => {
+    const a = document.createElement("a");
+    a.href = image;
+    const timestamp = new Date().toJSON().slice(0, 19); 
+    a.download = createFileName(extension, name + timestamp );
+    a.click();
+  };
+  const downloadScreenshot = () => takeScreenshot(ref.current).then(download);
 
   return <Box>
-    <Favicon url='https://img.icons8.com/plumpy/24/000000/visible--v1.png' />
-    <Game w={{ appWidth }} />
+
+    <div ref={ref}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <VisibilityTwoToneIcon />
+          </IconButton>
+          <Typography variant="h6" >JVoiPa</Typography>
+          <Box sx={{ flexGrow: 1 }} />
+          <IconButton onClick={downloadScreenshot} edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <SaveTwoToneIcon />
+          </IconButton>
+          <FeedbackFormDialog />
+          <AlertDialog />
+          <CopyrightDialog />
+          <ShareDialog />
+        </Toolbar>
+
+      </AppBar>
+      <Game w={{ appWidth }} />
+    </div>
+
+
   </Box>;
 }
